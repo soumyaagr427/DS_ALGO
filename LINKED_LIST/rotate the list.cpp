3ca9684a06fc70ee683e42e7b7f1069954972dc0
Soumya Agrawal <soumya.agr6@gmail.com>
2021-10-11 18:35:06 +0530

    rotation of list by given k

---
+++ LINKED_LIST/rotate the list.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+ // } Driver Code Ends
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+
+class Solution
+{
+    public:
+    //Function to rotate a linked list.
+    Node* rotate(Node* head, int k)
+    {
+        //two pointers to traverse
+        Node* prev=NULL;
+        Node* curr=head;
+        
+        
+        //iterating pointers till given k
+        while(k--)
+        {
+            prev=curr;
+            curr=curr->next;
+        }
+        
+        //if k=length and curr becomes null we return head
+        
+        if(curr==NULL)
+            return head;
+        
+        while(curr->next!=NULL)
+            curr=curr->next;
+        
+        
+        curr->next=head;
+        head=prev->next;
+        prev->next=NULL;
+        
+        
+        return head;
+    }
+};
+    
+
+
+// { Driver Code Starts.
+
+void printList(Node *n)
+{
+    while (n != NULL)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, val, k;
+        cin>>n;
+        
+        cin>> val;
+        Node *head = new Node(val);
+        Node *tail = head;
+        
+        for(int i=0; i<n-1; i++)
+        {
+            cin>> val;
+            tail->next = new Node(val);
+            tail = tail->next;
+        }
+        
+        cin>> k;
+        
+        Solution ob;
+        head = ob.rotate(head,k);
+        printList(head);
+    }
+    return 1;
+}
