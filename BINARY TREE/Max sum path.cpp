e5d79df41784b83b94df6a89254da7847e64e8e0
Soumya Agrawal <soumya.agr6@gmail.com>
2022-02-24 15:47:20 +0530

    used max height and max diameter

---
+++ BINARY TREE/Max sum path.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+ // } Driver Code Ends
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    
+    //this & SIGN matters alot as because of this only
+    //we are able to make changes on variable maxi
+    // and then return the value;
+    int maxpathsum(Node * root,int &maxi)
+    {
+        if(!root) return 0;
+        
+        //max of 0 beacuse in case both path gives -ve value
+        //its better to discard both thrn take any
+        int lh=max(0,maxpathsum(root->left,maxi));
+        int rh=max(0,maxpathsum(root->right,maxi));
+        
+     
+        //now this will return max height for each node
+        maxi=max(maxi,lh+rh+root->data);
+        
+        
+        // this will return max sum path either to consider
+        // left or right
+        return root->data+max(lh,rh);
+        
+    }
+    
+    int maxPathSum(Node* root)
+    {
+         int maxi=INT_MIN;
+         maxpathsum(root,maxi);
+         return maxi;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        S
